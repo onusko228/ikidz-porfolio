eda60861dce988b6eded5f8183ab5ca1e18e5635
onusko228 < >
2022-11-13 15:32:05 +0200

    added menu

---
+++ index.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector(".menu")
+const menuItem = document.querySelector(".header__nav-list")
+menu.addEventListener("click",()=>{
+    menuItem.classList.toggle("active")
+})
